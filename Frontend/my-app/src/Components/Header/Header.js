7efc25044702487cfbca67be8f75bfaf9734c08a
Jamie Codrington <jamie.codrington@thehutgroup.com>
2022-03-05 16:50:29 +0000

    fleshed out

---
+++ Frontend/my-app/src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Header.styles";
+
+export const Header = () => {
+  return (
+    <s.MainHeading>
+      <h1>Spotify Playlist Visualisation</h1>
+    </s.MainHeading>
+  );
+};
---
+++ Frontend/my-app/src/Components/Header/Header.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const MainHeading = styled.div`
+  margin: 2rem 0;
+  color: black;
+`;
+
+const s = {
+  MainHeading,
+};
+
+export default s;
