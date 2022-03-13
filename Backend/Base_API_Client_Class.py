77e70086cadffebe8a25a9c0e6dace9f2a01a039
Luke Veitch <luke.veitch@live.co.uk>
2022-03-13 13:59:19 +0000

    Creating class methods

--- Backend/Base_API_Client_Class.py
+++ Backend/Base_API_Client_Class.py
@@ -12,5 +12,47 @@ class SpotifyAPI(object):
 
     access_token = None
     access_token_expires = datetime.datetime.now()
+    access_token_did_expire = True
     token_url = "https://accounts.spotify.com/api/token"
 
+
+
+    def __init__(self, client_id, client_secret, *args, **kwargs): # always going to need the client id and secret.#
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id 
+        self.cliend_secret = client_secret
+
+    def encode_those_creds_bs4(self):
+        client_id = self.client_id
+        client_secret = self.client_secret
+        client_creds = f"{client_id}:{client_secret}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+
+        return client_creds_b64.decode()
+
+    def get_token_headers(self):
+        return {
+            "Authorization": f"Basic {self.encode_those_creds_bs4}" # <base64 encoded client_id:client_secret>
+        }
+
+    def get_token_data(self):
+        return {
+        "grant_type": "client_credentials"
+        }
+
+    def perform_request(self):
+        ########## Making a request
+        token_url = self.token_url
+        token_data = self.get_token_data()
+        token_headers = self.get_token_headers()
+        r = requests.post(token_url, data=token_data, headers=token_headers)
+
+        ########## Valid request + extract the data
+        if valid_request:
+            token_response_data = r.json()
+            now = datetime.datetime.now()
+            access_token = token_response_data['access_token']
+            expires_in = token_response_data['expires_in'] # seconds
+            expires = now + datetime.timedelta(seconds=expires_in)
+    did_expire = expires < now
+        
