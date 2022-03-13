a90ec385f493fecdcb24ada4d74daf09e1c675c2
Luke Veitch <luke.veitch@live.co.uk>
2022-03-13 15:32:40 +0000

    Successful authentication class

--- Backend/Base_API_Client_Class.py
+++ Backend/Base_API_Client_Class.py
@@ -15,12 +15,10 @@ class SpotifyAPI(object):
     access_token_did_expire = True
     token_url = "https://accounts.spotify.com/api/token"
 
-
-
-    def __init__(self, client_id, client_secret, *args, **kwargs): # always going to need the client id and secret.#
+    def __init__(self, client_id, client_secret, *args, **kwargs): # always need the client id and secret.#
         super().__init__(*args, **kwargs)
         self.client_id = client_id 
-        self.cliend_secret = client_secret
+        self.client_secret = client_secret
 
     def encode_those_creds_bs4(self):
         client_id = self.client_id
@@ -32,7 +30,7 @@ class SpotifyAPI(object):
 
     def get_token_headers(self):
         return {
-            "Authorization": f"Basic {self.encode_those_creds_bs4}" # <base64 encoded client_id:client_secret>
+            "Authorization": f"Basic {self.encode_those_creds_bs4()}" # <base64 encoded client_id:client_secret>
         }
 
     def get_token_data(self):
@@ -41,28 +39,31 @@ class SpotifyAPI(object):
         }
 
     def perform_request(self):
-        ########## Making a request
+
         token_url = self.token_url
         token_data = self.get_token_data()
         token_headers = self.get_token_headers()
+
+        ### Make the request
         r = requests.post(token_url, data=token_data, headers=token_headers)
         data = r.json()
-        if r.status_code in range(200,299):
-            return data
-        else:
-            raise Exception("Error with the request")
-        
-    
-    def perform_auth(self):
-        data = self.perform_request()
-        access_token = data['access_token']
+        if not r.status_code in range(200,299):
+            print(f"Error with the request\nRequest status code: {r.status_code}")
 
+        access_token = data['access_token']
         now = self.access_token_expires
         expires_in = data['expires_in'] # seconds
         expires = now + datetime.timedelta(seconds=expires_in)
 
         self.access_token_expires = expires
         self.access_token_did_expire = expires < now
-               
         self.access_token = access_token 
+        # if self.access_token_did_expire:
+        #     print(f"The access HaduToken expires at {self.access_token_expires}")
+        return "Authentication successful"
+ 
+
+spotify = SpotifyAPI(client_id, client_secret)
+print(spotify.perform_request())
+print(spotify.access_token_expires)
 
