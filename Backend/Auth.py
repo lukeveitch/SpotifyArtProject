50c662749ffa06d9a8b270bf151bed67968b7683
Luke Veitch <luke.veitch@live.co.uk>
2022-03-13 13:33:49 +0000

    More comments

--- Backend/Auth.py
+++ Backend/Auth.py
@@ -20,15 +20,16 @@ client_secret = '2cde9d2445884dd88f5b04ea5eb30887'
 
         #data needs to be parsed -- seperate issue
 
+
+
+########## Getting the client credentials
 client_creds = f"{client_id}:{client_secret}"
 client_creds_b64 = base64.b64encode(client_creds.encode())
 
 
+########## Getting the access haduToken
 token_url = "https://accounts.spotify.com/api/token"
-
-
 method = "POST"
-
 token_data = {
     "grant_type": "client_credentials"
 }
@@ -36,11 +37,13 @@ token_headers = {
     "Authorization": f"Basic {client_creds_b64.decode()}" # <base64 encoded client_id:client_secret>
 }
 
-
+########## Making a request
 r = requests.post(token_url, data=token_data, headers=token_headers)
 valid_request = r.status_code in range(200, 299)
 
 print(r.json())
+
+########## Valid request + extract the data
 if valid_request:
     token_response_data = r.json()
     now = datetime.datetime.now()
--- Backend/Base_API_Client_Class.py
+++ Backend/Base_API_Client_Class.py
@@ -7,5 +7,10 @@ client_id = 'a0ba4546898d4b79ba7b2eb7aaa3f3a5'
 client_secret = '2cde9d2445884dd88f5b04ea5eb30887'
 
 class SpotifyAPI(object):
+    client_id = None
+    client_secret = None
+
     access_token = None
-    
+    access_token_expires = datetime.datetime.now()
+    token_url = "https://accounts.spotify.com/api/token"
+
