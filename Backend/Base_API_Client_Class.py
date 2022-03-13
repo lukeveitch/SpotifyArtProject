bee9d2aa142cfc08daf1160528eb45c1aeda9a13
Luke Veitch <luke.veitch@live.co.uk>
2022-03-13 14:53:51 +0000

    v1 - bugs - not fully working

--- Backend/Base_API_Client_Class.py
+++ Backend/Base_API_Client_Class.py
@@ -11,11 +11,8 @@ class SpotifyAPI(object):
     client_secret = None
 
     access_token = None
-    access_token_expires = datetime.datetime.now()
     access_token_did_expire = True
     token_url = "https://accounts.spotify.com/api/token"
-
-
 
     def __init__(self, client_id, client_secret, *args, **kwargs): # always going to need the client id and secret.#
         super().__init__(*args, **kwargs)
@@ -26,46 +23,39 @@ class SpotifyAPI(object):
         client_id = self.client_id
         client_secret = self.client_secret
 
-        if client_secret == None or client_id == None:
-            raise Exception("You must give me a client_id and client_secret silly monke")
-
         client_creds = f"{client_id}:{client_secret}"
         client_creds_b64 = base64.b64encode(client_creds.encode())
 
         return client_creds_b64.decode()
 
     def get_token_headers(self):
+        client_credentials_bs4 = self.encode_those_creds_bs4()
         return {
-            "Authorization": f"Basic {self.encode_those_creds_bs4}" # <base64 encoded client_id:client_secret>
+            "Authorization": f"Basic {client_credentials_bs4}" # <base64 encoded client_id:client_secret>
         }
 
     def get_token_data(self):
         return {
-        "grant_type": "client_credentials"
+            "grant_type": "client_credentials"
         }
 
-    def perform_request(self):
-        ########## Making a request
+    def perform_auth(self):
         token_url = self.token_url
         token_data = self.get_token_data()
         token_headers = self.get_token_headers()
         r = requests.post(token_url, data=token_data, headers=token_headers)
+        if r.status_code not in range(200, 299):
+            return False
         data = r.json()
-        if r.status_code in range(200,299):
-            return data
-        else:
-            raise Exception("Error with the request")
-        
-    
-    def perform_auth(self):
-        data = self.perform_request()
+        now = datetime.datetime.now()
         access_token = data['access_token']
-
-        now = self.access_token_expires
         expires_in = data['expires_in'] # seconds
         expires = now + datetime.timedelta(seconds=expires_in)
-
+        self.access_token = access_token
         self.access_token_expires = expires
         self.access_token_did_expire = expires < now
-        self.access_token = access_token 
+        return True
 
+spotify = SpotifyAPI(client_id, client_secret)
+print(spotify.perform_auth())
+print(spotify.access_token_did_expire, spotify.access_token_expires)
