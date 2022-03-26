47df53febda43d9c29a7cc902e1dccd303c26519
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-03-26 13:09:25 +0000

    enviroment variables and spotipy auth

---
+++ Backend/auth.py
@@ -0,0 +1,11 @@
+import requests
+
+r = requests.get('')
+
+
+# client_id = 
+# response_type =
+# redirect_uri = https://www.bbc.co.uk/programmes/p02619x1
+# state = 
+scope = 'ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private user-read-email user-follow-modify user-follow-read user-library-modify user-library-read streaming app-remote-control user-read-playback-position user-top-read user-read-recently-played playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public'
+#show_dialog optional
--- Backend/main.py
+++ Backend/main.py
@@ -2,7 +2,13 @@
 # import requests
 
 import requests
-from spotify_token import token_auth
+from spotify_token import access
+
+endpoint = "https://api.spotify.com/v1/me"
+access_token = access
+r = requests.get(endpoint, headers = {"Accept": "application/json",
+                                        "Content-Type": "application/json",
+                                        "Authorization": "Bearer "})
 
 class QueryEndpoints:
     def __init__(self):
@@ -45,5 +51,5 @@ class QueryEndpoints:
         #return top 3 most common genres in a users playlist
         pass
 
-endpoints = QueryEndpoints
-print(endpoints.get_user_id())
+#endpoints = QueryEndpoints
+print(r.json())
