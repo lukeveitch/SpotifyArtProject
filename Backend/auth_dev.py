864f633e4337cd09d7e5fa0238d2978b782b582b
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-03-26 13:58:25 +0000

    parsing data and printing to terminal

--- Backend/auth_dev.py
+++ Backend/auth_dev.py
@@ -16,28 +16,59 @@ token = util.prompt_for_user_token(username,
                            client_secret=client_secret,
                            redirect_uri='https://puginarug.com/')
 
-#print(token)
+
 if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks()
     spotify_user = sp.current_user()
+    spotify_user_playlists = sp.current_user_playlists()
+    
+    # time_range = ['short_term', 'medium_term', 'long_term']
+    # for period in time_range:
+    #     print(period)
+    #     pprint.pprint(sp.current_user_top_tracks(time_range = period))
+    #time_range - Over what time frame are the affinities computed Valid-values: short_term, medium_term, long_term
+    # current_user_top_artists
+    # playlist
+    # playlist_cover_image
+    # playlist_tracks
+    # playlist_tracks
+
+
+
+    #User information variables
     name = spotify_user['display_name']
     number_of_followers = spotify_user['followers']['total']
     user_uri = spotify_user['uri']
     number_of_songs_saved = results['total']
+    number_of_playlists = spotify_user_playlists['total']
 
 
 
+    
     print(f'---------------------')
     print(f'Hi {name}. Welcome to this art spotify art project.')
     print(f'---------------------')
     print(f'You have {number_of_songs_saved} songs saved')
     print(f'---------------------')
+    print(f'You have {number_of_playlists} playlists')
     print(f'---------------------')
+    print(f'You have {number_of_playlists} playlists')
     print(f'---------------------')
+    print(f'You have {number_of_playlists} playlists')
     print(f'---------------------')
 
-    
+    for item in spotify_user_playlists['items']:  
+
+        #playlist information
+        playlist_id = item['id']
+        playlist_name = item['name']
+        playlist_owner = item['owner']['display_name']
+        number_of_playlist_tracks = item['tracks']['total']
+
+        
+        print(f'Playlist id: {playlist_id}\nName: {playlist_name}\nOwner: {playlist_owner}\nNumber of tracks: {number_of_playlist_tracks}')
+        print('---------------------')
 
 
 #     for item in results['items']:
