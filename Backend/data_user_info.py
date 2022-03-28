44b8977afdf97a930c929ae1a2419588444b678e
Luke Veitch <luke.veitch@live.co.uk>
2022-03-28 16:30:09 +0100

    top artists and tracks - long term

--- Backend/data_user_info.py
+++ Backend/data_user_info.py
@@ -1,13 +1,18 @@
 from auth import token
 import spotipy
+import pprint
 
 sp = spotipy.Spotify(auth=token)
 results = sp.current_user_saved_tracks()
 spotify_user = sp.current_user()
 spotify_user_playlists = sp.current_user_playlists()
-
-
-# current_user_top_artists
+top_artists = sp.current_user_top_artists(time_range = "long_term")
+top_tracks = sp.current_user_top_tracks(time_range = "long_term")
+for count, items in enumerate(top_artists['items']):
+    print(count+1, items['name'])
+print('-----------')
+for count, items in enumerate(top_tracks['items']):
+    print(count+1, items['name'])
 # playlist
 # playlist_cover_image
 # playlist_tracks
@@ -25,43 +30,41 @@ number_of_playlists = spotify_user_playlists['total']
 
 
 
-print(f'---------------------')
-print(f'Hi {name}. Welcome to this art spotify art project.')
-print(f'---------------------')
-print(f'You have {number_of_songs_saved} songs saved')
-print(f'---------------------')
-print(f'You have {number_of_playlists} playlists')
-print(f'---------------------')
-print(f'You have {number_of_playlists} playlists')
-print(f'---------------------')
-print(f'You have {number_of_playlists} playlists')
-print(f'---------------------')
+# print(f'---------------------')
+# print(f'Hi {name}. Welcome to this art spotify art project.')
+# print(f'---------------------')
+# print(f'You have {number_of_songs_saved} songs saved')
+# print(f'---------------------')
+# print(f'You have {number_of_playlists} playlists')
+# print(f'---------------------')
+# print(f'---------------------')
+# print(f'---------------------')
 
-playlist_info = input('Would you like more information about your playlists? y/n\n>>')
+#playlist_info = input('Would you like more information about your playlists? y/n\n>>')
 
-if playlist_info == 'y':
+#if playlist_info == 'y':
 
-    playlists_less_than_ten = {}
+# playlists_less_than_ten = {}
 
-    for item in spotify_user_playlists['items']:  
-            
-        #playlist information
-        playlist_id = item['id']
-        playlist_name = item['name']
-        playlist_owner = item['owner']['display_name']
-        number_of_playlist_tracks = item['tracks']['total']
-
-        if number_of_playlist_tracks <= 10:
-            playlists_less_than_ten[playlist_name] = playlist_id
+# for item in spotify_user_playlists['items']:  
         
-        print(f'Playlist id: {playlist_id}\nName: {playlist_name}\nCreated by: {playlist_owner}\nNumber of tracks: {number_of_playlist_tracks}')
-        print('---------------------')
+#     #playlist information
+#     playlist_id = item['id']
+#     playlist_name = item['name']
+#     playlist_owner = item['owner']['display_name']
+#     number_of_playlist_tracks = item['tracks']['total']
 
-    num = len(playlists_less_than_ten)
-    print(f'You have {num} playlists that are less than 10 tracks.')
-    print(playlists_less_than_ten)
+#     if number_of_playlist_tracks <= 10:
+#         playlists_less_than_ten[playlist_name] = playlist_id
+    
+#     print(f'Playlist id: {playlist_id}\nName: {playlist_name}\nCreated by: {playlist_owner}\nNumber of tracks: {number_of_playlist_tracks}')
+#     print('---------------------')
 
-elif playlist_info == 'n': 
-    print('Stop being a fat cunt')
-else:
-    pass
+# num = len(playlists_less_than_ten)
+# print(f'You have {num} playlists that are less than 10 tracks.')
+# print(playlists_less_than_ten)
+
+# elif playlist_info == 'n': 
+#     print('Stop being a fat cunt')
+# else:
+#     pass
