2132bdd695b35c4f1e3bc4b9d1d1d41c62b03dbf
Luke Veitch <luke.veitch@live.co.uk>
2022-03-30 12:11:37 +0100

    creating entry points

--- Backend/endpoints.py
+++ Backend/endpoints.py
@@ -6,3 +6,21 @@ import ast
 app = Flask(__name__)
 api = Api(app)
 
+class Playlists(Resource):
+    # methods go here
+    pass
+
+class topArtists(Resource):
+    # methods go here
+    pass
+
+class topTracks(Resource):
+    # methods go here
+    pass
+
+api.add_resource(Playlists, '/playlists') # returns all playlists, playlistImage, playlistName, numberOfTracks, playlistID
+api.add_resource(Playlists, '/playlists/{playlistId}') # returns specific playlist and top genres
+
+api.add_resource(topArtists, '/topArtists') #returns top artists short-term, medium and long term 
+api.add_resource(topTracks, '/topTracks')   #returns top tracks short-term, medium and long term 
+
