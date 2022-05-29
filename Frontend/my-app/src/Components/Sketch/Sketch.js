80b6b9799f9143225d230aa2670612f0826e539a
Jamie Codrington <jamie.codrington@thehutgroup.com>
2022-05-29 13:00:01 +0100

    animation and circle added

--- Frontend/my-app/src/Components/Sketch/Sketch.js
+++ Frontend/my-app/src/Components/Sketch/Sketch.js
@@ -1,11 +1,12 @@
 function Sketch(p) {
   let dimension = 800;
-  let backgroundColor = 0;
+  // let backgroundColor = 0;
 
   p.setup = function () {
     p.createCanvas(dimension, dimension);
-    p.background(backgroundColor);
-    p.blendMode(p.ADD)
+    // p.background(backgroundColor);
+    p.blendMode(p.BURN)
+    p.frameRate(4);
 
     let c1 = "#ffb2b2"
     let c2 = "#9affe6"
@@ -20,37 +21,64 @@ function Sketch(p) {
     p.square(0, 0, dimension);
     p.pop();
 
-    //squares generation
     p.push();
-    let i = 0;
-    while (i <= 11) {
-      let j = 0;
-      while (j <= 11) {
-            let posX = i * 80;
-            let posY = j * 80;
-            let dist = p.random(35);
-
-            let squareSize = p.random(50);
-            let squareSize2 = p.random(50);
-            let squareSize3 = p.random(50);
-
-            p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
-            p.rect(posX, posY, squareSize, squareSize2);
-            p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
-            p.rect(posX + dist, posY + dist, squareSize3, squareSize);
-            p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
-            p.rect(posX + dist * 2, posY + dist * 2, squareSize3, squareSize2);
-        j += 1;
-      }
-      i += 1;
-    }
+    p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    p.circle(Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension/ 4))
+    p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    p.circle(Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension/ 3))
+    p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    p.circle(Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension), Math.floor(Math.random() * dimension / 2))
     p.pop();
+
+    //squares generation
+    // p.push();
+    // let i = 0;
+    // while (i <= 11) {
+    //   let j = 0;
+    //   while (j <= 11) {
+    //         let posX = i * 80;
+    //         let posY = j * 80;
+    //         let dist = p.random(35);
+    //
+    //         let squareSize = p.random(50);
+    //         let squareSize2 = p.random(50);
+    //         let squareSize3 = p.random(50);
+    //
+    //         p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    //         p.rect(posX, posY, squareSize, squareSize2);
+    //         p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    //         p.rect(posX + dist, posY + dist, squareSize3, squareSize);
+    //         p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+    //         p.rect(posX + dist * 2, posY + dist * 2, squareSize3, squareSize2);
+    //     j += 1;
+    //   }
+    //   i += 1;
+    // }
+    // p.pop();
 
 
 
 
 
     p.draw = () => {
+
+      let i = Math.floor(Math.random() * 11);
+      let j = Math.floor(Math.random() * 11);
+
+      let posX = i * 80;
+      let posY = j * 80;
+      let dist = p.random(30);
+
+      let squareSize = p.random(50);
+      let squareSize2 = p.random(50);
+      let squareSize3 = p.random(50);
+
+      p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+      p.rect(posX, posY, squareSize, squareSize2);
+      p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+      p.rect(posX - dist, posY + dist, squareSize3, squareSize);
+      p.fill(coloursArray[Math.floor(Math.random() * coloursArray.length)]);
+      p.rect(posX + dist * 2, posY + dist * 2, squareSize3, squareSize2);
 
     };
   }
